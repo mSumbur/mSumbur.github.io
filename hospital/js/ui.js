ee43bf18a51d88da655bad6d259ca6f89d84145b
sumbur <m.sumbur@outlook.com>
2020-12-06 15:55:36 +0800

    完成

--- hospital/js/ui.js
+++ hospital/js/ui.js
@@ -62,13 +62,54 @@ $.fn.UiBackUp = function() {
 // ui-slider
 $.fn.UiSlider = function() {
     var ui = $(this)
-    var item = $('.ui-slider-wrap .item', ui)
+    var wrap = $('.ui-slider-wrap')
+    var items = $('.ui-slider-wrap .item', ui)
     var btn_prev = $('.ui-slider-arrow .left', ui)
     var btn_next = $('.ui-slider-arrow .right', ui)
     var process = $('.ui-slider-process .item', ui)
 
-    btn_prev.on('click', function() {
+    var current = 0
+    var size = items.size()
+    var width = items.eq(0).width()
+    var enableAuto = true
 
+    ui.on('mouseover', function() {
+        enableAuto = false
+    }).on('mouseout', function() {
+        enableAuto = true
+    })
+
+    wrap.on('move_prev', function() {
+        if (current <= 0) {
+            current = size
+        }
+        current -= 1
+        wrap.triggerHandler('move_to', current)
+    }).on('move_next', function() {
+        if (current >= size - 1) {
+            current = -1
+        }
+        current += 1
+        wrap.triggerHandler('move_to', current)
+    }).on('move_to', function(e, index) {
+        wrap.css('left', index * width * -1)
+        process.removeClass('item_focus').eq(index).addClass('item_focus')
+    }).on('auto_move', function() {
+        setInterval(function() {
+            enableAuto && wrap.triggerHandler('move_next')
+        }, 2000)
+    }).triggerHandler('auto_move')
+
+    btn_prev.on('click', function() {
+        wrap.triggerHandler('move_prev')
+    })
+    btn_next.on('click', function() {
+        wrap.triggerHandler('move_next')
+    })
+    process.on('click', function() {
+        var index = $(this).index()
+        current = index
+        wrap.triggerHandler('move_to', index)
     })
 }
 
